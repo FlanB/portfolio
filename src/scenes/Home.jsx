ac485186b71fe6e460241563427042592c9d925f
FlanB <clement.renou@mmibordeaux.com>
2021-11-15 01:14:36 +0100

    ✨ progress bar

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,55 +1,81 @@
 import "./_Home.scss"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import { Link } from "react-router-dom"
 
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import { Container, Row, Col } from "react-bootstrap"
 
 import projects from "../projects.json"
 
-function Home() {
-  const [progressBarWidth, setProgressBarWidth] = useState(0)
+let index = 0
+let once = false
+let projectsList = []
 
+function Home() {
+  const ProgressBar = useRef(null)
+  const [progressBarWidth, setProgressBarWidth] = useState(0)
   useEffect(() => {
+    projects.forEach((project, index) => {
+      projectsList.push(document.getElementById(index))
+    })
+    for (let id = 1; id < projectsList.length; id++) {
+      projectsList[id].style.display = "none"
+    }
     setInterval(
       () => setProgressBarWidth((progressBarWidth) => progressBarWidth + 10),
       1000
     )
   }, [])
-  if (progressBarWidth >= 100) {
-    setProgressBarWidth(0)
-    setTimeout(() => console.log("endTransition"), 1000)
+
+  if (progressBarWidth >= 100 && once === false) {
+    once = true
+    projectsList[index].style.display = "none"
+    if (index === projectsList.length - 1) {
+      index = 0
+    } else {
+      index++
+    }
+    projectsList[index].style.display = null
+    setTimeout(() => {
+      ProgressBar.current.style.transition = "unset"
+      setProgressBarWidth(0)
+      once = false
+      ProgressBar.current.style.transition = null
+    }, 500)
   }
 
   return (
     <>
       <Container className="home">
-        <Row className="content">
-          <Col md={{ span: 7 }}>
-            <img src={projects[0].imgs[0]} alt="" />
-          </Col>
-          <Col className="home-info" md={{ span: 5 }}>
-            <div className="home-text">
-              <h1 className="home-display">{projects[0].name}</h1>
-              <p>{projects[0].description}</p>
-            </div>
-            <div className="home-interaction">
-              <Link to="/project/makyma" className="red btn">
-                En savoir plus
-              </Link>
-              <div className="line"></div>
-            </div>
-          </Col>
-        </Row>
+        {projects.map((project, index) => (
+          <Row className="content" key={index} id={index}>
+            <Col md={{ span: 7 }}>
+              <img src={project.imgs[0]} alt="" />
+            </Col>
+            <Col className="home-info" md={{ span: 5 }}>
+              <div className="home-text">
+                <h1 className="home-display">{project.name}</h1>
+                <p>{project.description}</p>
+              </div>
+              <div className="home-interaction">
+                <Link to="/project/makyma" className="red btn">
+                  En savoir plus
+                </Link>
+                <div className="line"></div>
+              </div>
+            </Col>
+          </Row>
+        ))}
         <Row className="slider">
           <Col className="line-container" md={{ span: 4 }}>
-            01
+            0{index + 1}
             <div className="line">
-              <span style={{ width: progressBarWidth + "%" }}></span>
+              <span
+                ref={ProgressBar}
+                style={{ width: progressBarWidth + "%" }}
+              ></span>
             </div>
-            03
+            0{projects.length}
           </Col>
         </Row>
       </Container>
