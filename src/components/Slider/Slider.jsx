260e7ef7dedf92c0bb43473360564aa97fbea9f5
FlanB <clement.renou@mmibordeaux.com>
2021-11-24 22:47:54 +0100

    🐛 fix slide animation

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -5,6 +5,7 @@ import { useState, useRef, useEffect, useContext } from "react"
 import { projectsList, Context } from "../../scenes/Home"
 
 let once = false
+let once2 = false
 
 export default function Slider() {
   const { index, setIndex, skill } = useContext(Context)
@@ -12,7 +13,7 @@ export default function Slider() {
   const ProgressBar = useRef(null)
   useEffect(() => {
     setProgressBarWidth(0)
-  }, [skill])
+  }, [skill, index])
   useEffect(() => {
     setInterval(
       () => setProgressBarWidth((progressBarWidth) => progressBarWidth + 10),
@@ -23,29 +24,53 @@ export default function Slider() {
   useEffect(() => {
     if (progressBarWidth >= 100 && once === false) {
       once = true
-      projectsList[index].style.display = "none"
       if (index === projectsList.length - 1) {
         setIndex(0)
       } else {
         setIndex(index + 1)
       }
-      if (index < projectsList.length - 1) {
-        projectsList[index + 1].style.display = null
-      } else {
-        projectsList[0].style.display = null
-      }
-      //   ProgressBar.current.style.transition = 'unset'
-      setProgressBarWidth(0)
-      //   ProgressBar.current.style.transition = null
       setTimeout(() => {
         once = false
       }, 500)
     }
   }, [index, progressBarWidth, setIndex])
 
+  useEffect(() => {
+    if (once2 === true) {
+      if (index === 1) {
+        projectsList[0].style.display = "none"
+      } else if (index === 0) {
+        projectsList[projectsList.length - 1].style.display = "none"
+      } else {
+        projectsList[index - 1].style.display = "none"
+      }
+
+      projectsList[index].style.display = null
+    }
+    once2 = true
+  }, [index])
+
   return (
     <Row className="slider">
       <Col className="line-container" md={{ span: 4 }}>
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          strokeWidth="2"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          onClick={() => {
+            if (index > 0) {
+              setIndex(index - 1)
+              setProgressBarWidth(0)
+            }
+          }}
+        >
+          <polyline points="15 18 9 12 15 6"></polyline>
+        </svg>
         0{index + 1}
         <div className="line">
           <span
@@ -54,6 +79,24 @@ export default function Slider() {
           ></span>
         </div>
         0{projectsList.length}
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          strokeWidth="2"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          onClick={() => {
+            if (index < projectsList.length - 1) {
+              setIndex(index + 1)
+              setProgressBarWidth(0)
+            }
+          }}
+        >
+          <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
       </Col>
     </Row>
   )
